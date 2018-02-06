2c2bc355213c9fb216d9b9a92b377ef66b181a36
Caleb Shea <caleb.shea11@gmail.com>
2018-02-06 11:53:19 -0600

    Slight edit

--- Stair Calculator/src/Main.java
+++ Stair Calculator/src/Main.java
@@ -51,8 +51,8 @@ public class Main {
 		memo[0] = 1;
 		memo[1] = 1;
 		if(n > 1)
-			memo[2] = 2;
 		{
+			memo[2] = 2;
 			for(int i = 3; i < n + 1; i++)
 			{
 				memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
